a28b08a173089de6c16dd85650b78617aeeab32d
Jiyoung Kim <ehlonna@naver.com>
2020-06-04 00:09:42 +0900

    Spring MVC Practice

--- spring/src/main/java/study/spring/myapp/hr/controller/EmpController.java
+++ spring/src/main/java/study/spring/myapp/hr/controller/EmpController.java
@@ -1,11 +1,16 @@
 package study.spring.myapp.hr.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import study.spring.myapp.hr.dao.IEmpService;
+import study.spring.myapp.hr.model.EmpVO;
 
 @Controller
 public class EmpController {
@@ -13,10 +18,34 @@ public class EmpController {
 	@Autowired
 	IEmpService empService;
 	
-	@GetMapping("/hr/jiyoung")
-	public String getHigherSalary(Model model) {
-		model.addAttribute("higherSalary", empService.getHigherSalary());
-		return "home";
+//	@GetMapping("/hr/jiyoung")
+//	public String getHigherSalary(Model model) {
+//		model.addAttribute("higherSalary", empService.getHigherSalary());
+//		return "home";
+//	}
+	
+	@RequestMapping(value="/hr/count")
+	public String empCount(@RequestParam(value="deptId", required=false, defaultValue="0") int deptId, Model model) {
+		if(deptId == 0) {
+			model.addAttribute("count", empService.getEmpCount());
+		} else {
+			model.addAttribute("count", empService.getEmpCount(deptId));
+		}
+		return "hr/count";
+	}
+	
+	@RequestMapping(value="/hr/list")
+	public String getAllEmployees(Model model) {
+		List<EmpVO> empList = empService.getEmpList();
+		model.addAttribute("empList", empList);
+		return "hr/list";
+	}
+	
+	@RequestMapping(value="/hr/{employeeId}")
+	public String getEmployees(@PathVariable int employeeId, Model model) {
+		EmpVO emp = empService.getEmpInfo(employeeId);
+		model.addAttribute("emp", emp);
+		return "hr/view";
 	}
 	
 
