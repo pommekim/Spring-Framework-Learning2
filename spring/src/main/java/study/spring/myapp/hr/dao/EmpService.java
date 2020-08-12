aed150ee192c9bf94911fe9679fb3bc562b03c4f
Jiyoung Kim <ehlonna@naver.com>
2020-08-12 18:20:24 +0900

    Mybatis Basic

--- spring/src/main/java/study/spring/myapp/hr/dao/EmpService.java
+++ spring/src/main/java/study/spring/myapp/hr/dao/EmpService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import study.spring.myapp.hr.model.DeptVO;
@@ -13,6 +14,7 @@ import study.spring.myapp.hr.model.EmpVO;
 public class EmpService implements IEmpService {
 	
 	@Autowired
+	@Qualifier("IEmpRepository")
 	IEmpRepository empRepository;
 
 	@Override
